183b47becf1472f4f6e6c1a8e8f46ce30ca8faae
Sean Hendrix <hendoware@gmail.com>
2019-03-15 17:04:12 -0400

    add setup viewcontroller swift file

---
+++ Aion Diet/Setup View Controllers/SetupViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SetupViewController.swift
+//  Aion Diet
+//
+//  Created by Sean Hendrix on 3/15/19.
+//  Copyright © 2019 Sean Hendrix. All rights reserved.
+//
+
+import UIKit
+
+class SetupViewController: UIViewController {
+    
+    
+    
+    
+}
